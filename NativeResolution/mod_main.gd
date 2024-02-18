7a6efdaabffe7260c54293730a8262c7f30d37d8
Emil <50961259+EmK530@users.noreply.github.com>
2024-02-18 12:44:27 +0100

    Add files via upload

---
+++ NativeResolution/mod_main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var root = null
+
+func _process(delta):
+    if root == null:
+        root = get_tree().get_root()
+    else:
+        root.content_scale_factor=root.get_size()[1]/540
+        root.content_scale_mode=(1 if DisplayServer.window_get_mode()==0 else 0)
