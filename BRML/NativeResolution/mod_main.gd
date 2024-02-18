e75a8562c76c9facd07a39a18dcae1dbdb1dbdcd
Emil <50961259+EmK530@users.noreply.github.com>
2024-02-18 14:17:32 +0100

    Fix maximized mode

--- BRML/NativeResolution/mod_main.gd
+++ BRML/NativeResolution/mod_main.gd
@@ -7,4 +7,4 @@ func _process(delta):
         root = get_tree().get_root()
     else:
         root.content_scale_factor=root.get_size()[1]/540
-        root.content_scale_mode=(1 if DisplayServer.window_get_mode()==0 else 0)
+        root.content_scale_mode=(0 if DisplayServer.window_get_mode()==4 else 1)
