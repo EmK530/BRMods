af394abb2d5db6bdb2d7dd6dbc9c809c75c7a0ac
Emil <50961259+EmK530@users.noreply.github.com>
2024-03-07 08:41:41 +0100

    Add files via upload

--- BRML/NativeResolution/mod_main.gd
+++ BRML/NativeResolution/mod_main.gd
@@ -6,10 +6,5 @@ func _process(delta):
     if root == null:
         root = get_tree().get_root()
     else:
-        var height=root.get_size()[1]
-        if DisplayServer.window_get_mode()==4 && height%540==0:
-            root.content_scale_mode=0
-            root.content_scale_factor=height/540
-        else:
-            root.content_scale_mode=1
-            root.content_scale_factor=1
+        root.content_scale_mode=1
+        root.content_scale_factor=1
