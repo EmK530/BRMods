c2e1aca38f89c184bc7a34c704387a818ac09684
Emil <50961259+EmK530@users.noreply.github.com>
2024-03-07 20:55:12 +0100

    Better posterization covering

--- BRML/NativeResolution/mod_main.gd
+++ BRML/NativeResolution/mod_main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var root = null
+var post_name = "Camera/post processing/posterization test"
 
 func _process(delta):
     if root == null:
@@ -8,3 +9,7 @@ func _process(delta):
     else:
         root.content_scale_mode=1
         root.content_scale_factor=1
+        if root.get_child(2).has_node(post_name):
+            var post = root.get_child(2).get_node(post_name)
+            post.position = Vector2i(-10,-10)
+            post.size = Vector2i(60,60)
