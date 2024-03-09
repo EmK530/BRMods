baa2cf67a9bfefb3ee700bbe073da42058adbddf
Emil <50961259+EmK530@users.noreply.github.com>
2024-03-10 00:03:20 +0100

    Widescreen w/ RenderScale + Config file

--- BRML/NativeResolution/mod_main.gd
+++ BRML/NativeResolution/mod_main.gd
@@ -1,15 +1,79 @@
 extends Node
 
+const AUTHORNAME_MODNAME_DIR := "EmK530-NativeResolution"
+const AUTHORNAME_MODNAME_LOG_NAME := "EmK530-NativeResolution:Main"
+
+const logname = "NativeResolution"
+
+var config = {
+    "upscaleResolution": true,
+    "wideScreen": false,
+    "renderScale": 1
+}
+
 var root = null
 var post_name = "Camera/post processing/posterization test"
+
+func pr(tx):
+    ModLoaderLog.info(tx,logname)
 
 func _process(delta):
     if root == null:
         root = get_tree().get_root()
+        var game_dir = OS.get_executable_path().get_base_dir()
+        var config_dir = game_dir+"/config/"
+        if FileAccess.file_exists(config_dir+AUTHORNAME_MODNAME_DIR+".txt"):
+            var file = FileAccess.open(config_dir+AUTHORNAME_MODNAME_DIR+".txt", FileAccess.READ)
+            pr("Config exists, reading...")
+            var ln = file.get_line()
+            while ln!="":
+                if ln.substr(0,1)!="#":
+                    var set = ln.split("=")
+                    if float(set[1])!=0:
+                        config[set[0]] = float(set[1])
+                    else:
+                        config[set[0]] = (true if set[1].to_lower()=="true" else false)
+                ln = file.get_line()
+            file.close()
+        else:
+            pr("Config does not exist, creating...")
+            var da = DirAccess.open(game_dir)
+            if not da.dir_exists("config"):
+                da.make_dir("config")
+            var file = FileAccess.open(config_dir+AUTHORNAME_MODNAME_DIR+".txt", FileAccess.WRITE)
+            file.store_string("upscaleResolution="+("true" if config["upscaleResolution"] else "false"))
+            file.store_string("\nwideScreen="+("true" if config["wideScreen"] else "false"))
+            file.store_string("\nrenderScale="+str(config["renderScale"]))
+            file.store_string("\n# Render scale is currently only available for wideScreen")
+            file.store_string("\n# Enabling the setting will override upscaleResolution")
+            file.store_string("\n# -1: Match window resolution\n# 1: 960x540 (Vanilla)\n# 1.5: 1440x810 (1.5x scale)\n# 2: 1920x1080 (2x scale)\n# 4: 3840x2160 (4x scale)\n#\n# And so on...")
+            file.close()
+        pr("Ready for action.")
+        if config["upscaleResolution"]:
+            root.content_scale_mode=1
     else:
-        root.content_scale_mode=1
-        root.content_scale_factor=1
-        if root.get_child(2).has_node(post_name):
-            var post = root.get_child(2).get_node(post_name)
-            post.position = Vector2i(-10,-10)
-            post.size = Vector2i(60,60)
+        if config["wideScreen"]:
+            var sz = root.get_size()
+            var szX = float(sz[0])
+            var szY = float(sz[1])
+            var mul = (540.0/szY) * (config["renderScale"])
+            if config["renderScale"] == -1:
+                mul = 1
+            var aspectX = szX/szY
+            var canvSize = szY/540.0*mul
+            var relAspX = (aspectX-(16.0/9.0))
+            root.content_scale_size = Vector2i(szX*mul,szY*mul)
+            root.set_canvas_transform(Transform2D(Vector2(canvSize,0),Vector2(0,canvSize),Vector2((relAspX/2.0)*szY*mul,0)))
+            if root.get_child(2).has_node(post_name):
+                var post = root.get_child(2).get_node(post_name)
+                var calcOffset = -10-relAspX*szX/2
+                if calcOffset > -10:
+                    calcOffset = -10
+                post.position = Vector2(calcOffset,-10)
+                post.scale = Vector2(aspectX*2.0,1)
+        elif config["upscaleResolution"]:
+            root.content_scale_factor=1
+            if root.get_child(2).has_node(post_name):
+                var post = root.get_child(2).get_node(post_name)
+                post.position = Vector2i(-10,-10)
+                post.size = Vector2i(60,60)
