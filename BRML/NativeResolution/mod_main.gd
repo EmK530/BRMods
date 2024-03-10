00d308406208a775dc6370e6e27cf29d3d5daaeb
Emil <50961259+EmK530@users.noreply.github.com>
2024-03-11 00:42:32 +0100

    Fix viewblockers

--- BRML/NativeResolution/mod_main.gd
+++ BRML/NativeResolution/mod_main.gd
@@ -12,10 +12,28 @@ var config = {
 }
 
 var root = null
+var copy_parent = "Camera/dialogue UI/viewblocker parent"
 var post_name = "Camera/post processing/posterization test"
+var viewBlocker = copy_parent+"/viewblocker"
+var customBlock = copy_parent+"/customBlock"
+var forceCoverTargets = [
+    post_name,
+    viewBlocker,
+    copy_parent+"/viewblcoker", # lmao
+    customBlock
+]
+var ratchet = copy_parent+"/ratchet"
+
+var title_passed = false
 
 func pr(tx):
     ModLoaderLog.info(tx,logname)
+
+func safe_get(name):
+    if root.get_child(2).has_node(name):
+        return root.get_child(2).get_node(name)
+    else:
+        return false
 
 func _process(delta):
     if root == null:
@@ -48,11 +66,33 @@ func _process(delta):
             file.store_string("\n# Enabling the setting will override upscaleResolution")
             file.store_string("\n# -1: Match window resolution\n# 1: 960x540 (Vanilla)\n# 1.5: 1440x810 (1.5x scale)\n# 2: 1920x1080 (2x scale)\n# 4: 3840x2160 (4x scale)\n#\n# And so on...")
             file.close()
+        if config["wideScreen"]:
+            pr("Creating custom viewblocker for menu.")
+            var par = safe_get(copy_parent)
+            if par:
+                var blk = safe_get(viewBlocker)
+                if blk:
+                    blk = blk.duplicate()
+                    blk.name = "customBlock"
+                    blk.color = Color(0,0,0,1)
+                    blk.z_index = -1
+                    par.add_child(blk)
+                else:
+                    pr("[WARN] Could not find viewblocker in the first frame.")
+            else:
+                pr("[WARN] Could not find viewblocker parent in the first frame.")
         pr("Ready for action.")
         if config["upscaleResolution"]:
             root.content_scale_mode=1
     else:
         if config["wideScreen"]:
+            if not title_passed:
+                var blk = safe_get(customBlock)
+                if blk:
+                    var rat = safe_get(ratchet)
+                    if not rat.visible:
+                        title_passed = true
+                        blk.queue_free()
             var sz = root.get_size()
             var szX = float(sz[0])
             var szY = float(sz[1])
@@ -64,16 +104,17 @@ func _process(delta):
             var relAspX = (aspectX-(16.0/9.0))
             root.content_scale_size = Vector2i(szX*mul,szY*mul)
             root.set_canvas_transform(Transform2D(Vector2(canvSize,0),Vector2(0,canvSize),Vector2((relAspX/2.0)*szY*mul,0)))
-            if root.get_child(2).has_node(post_name):
-                var post = root.get_child(2).get_node(post_name)
-                var calcOffset = -10-relAspX*szX/2
-                if calcOffset > -10:
-                    calcOffset = -10
-                post.position = Vector2(calcOffset,-10)
-                post.scale = Vector2(aspectX*2.0,1)
+            for i in forceCoverTargets:
+                var targ = safe_get(i)
+                if targ:
+                    var calcOffset = -10-relAspX*szX/2
+                    if calcOffset > -10:
+                        calcOffset = -10
+                    targ.position = Vector2(calcOffset,-10)
+                    targ.scale = Vector2(aspectX*2.0,1)
         elif config["upscaleResolution"]:
             root.content_scale_factor=1
-            if root.get_child(2).has_node(post_name):
-                var post = root.get_child(2).get_node(post_name)
+            var post = safe_get(forceCoverTargets[0])
+            if post:
                 post.position = Vector2i(-10,-10)
                 post.size = Vector2i(60,60)
