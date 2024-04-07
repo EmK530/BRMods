54495e46f90641248bc05a9c2f71fd3752a63755
Emil <50961259+EmK530@users.noreply.github.com>
2024-04-07 13:10:17 +0200

    Update LaunchNativeResolution.bat

--- Batch/NativeResolution/LaunchNativeResolution.bat
+++ Batch/NativeResolution/LaunchNativeResolution.bat
@@ -1,6 +1,7 @@
 @echo off
 cls
-echo extends SceneTree;func _init():change_scene_to_file('res://scenes/menu.tscn'); > "%temp%\BuckshotNativeRes.gd"
-echo func _process(delta):root.content_scale_factor=root.get_size()[1]/540;root.content_scale_mode=(0 if DisplayServer.window_get_mode()==4 else 1); >> "%temp%\BuckshotNativeRes.gd"
-start /B "" "Buckshot Roulette.exe" --script %temp%\BuckshotNativeRes.gd
+if exist "%cd%\NativeResolution.gd" goto:skip
+echo NativeResolution not found, downloading...
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/EmK530/BRMods/main/Batch/NativeResolution/mod_main.gd', 'NativeResolution.gd')"
+start /B "" "Buckshot Roulette.exe" --script "%cd%\NativeResolution.gd"
 exit
