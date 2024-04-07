3f436e13d9c654aeadc475954e3f498987567760
Emil <50961259+EmK530@users.noreply.github.com>
2024-04-07 13:12:59 +0200

    whoops

--- Batch/NativeResolution/LaunchNativeResolution.bat
+++ Batch/NativeResolution/LaunchNativeResolution.bat
@@ -3,5 +3,6 @@ cls
 if exist "%cd%\NativeResolution.gd" goto:skip
 echo NativeResolution not found, downloading...
 powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/EmK530/BRMods/main/Batch/NativeResolution/mod_main.gd', 'NativeResolution.gd')"
+:skip
 start /B "" "Buckshot Roulette.exe" --script "%cd%\NativeResolution.gd"
 exit
