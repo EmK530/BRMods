9276a16efb032bae0180d0464b2cfa2c9bfa25d3
MSLaFaver <scientific_guy@outlook.com>
2026-03-22 03:51:35 -0400

    Refactor and update for BRML Neo

--- BRML/NativeResolution/mod_main.gd
+++ BRML/NativeResolution/mod_main.gd
@@ -5,10 +5,11 @@ const AUTHORNAME_MODNAME_LOG_NAME := "EmK530-NativeResolution:Main"
 
 const logname = "NativeResolution"
 
+const vanilla_size = Vector2(960,540)
+
 var config = {
-    "upscaleResolution": true,
-    "wideScreen": true,
-    "renderScale": -1
+	"wideScreen": true,
+	"renderScale": 1
 }
 
 var root = null
@@ -16,116 +17,84 @@ var copy_parent = "Camera/dialogue UI/viewblocker parent"
 var post_name = "Camera/post processing/posterization test"
 var viewBlocker = copy_parent+"/viewblocker"
 var customBlock = copy_parent+"/customBlock"
-var forceCoverTargets = [
-    post_name,
-    viewBlocker,
-    copy_parent+"/viewblcoker", # lmao
-    customBlock,
-    "Camera/post processing/opengl post processing"
-]
 var ratchet = copy_parent+"/ratchet"
-
+var forceCoverTargets = [
+	post_name,
+	viewBlocker,
+	copy_parent+"/viewblcoker", # lmao
+	customBlock,
+	"Camera/post processing/opengl post processing"
+]
 var title_passed = false
 
 func pr(tx):
-    ModLoaderLog.info(tx,logname)
+	ModLoaderLog.info(tx,logname)
 
 func safe_get(name):
-    if root.get_child(7).has_node(name):
-        return root.get_child(7).get_node(name)
-    else:
-        return false
+	var node = root.get_children().back()
+	if node.has_node(name):
+		return node.get_node(name)
+	else:
+		return false
 
 var lastScene = ""
 
+func _ready():
+	ModLoader.get_node("MSLaFaver-ModMenu").config_init(AUTHORNAME_MODNAME_DIR, config)
+	root = get_tree().get_root()
+	var config_object = ModLoaderConfig.get_config(AUTHORNAME_MODNAME_DIR, "user")
+	if config_object != null:
+		config = config_object.data
+	pr("Ready for action.")
+
 func _process(delta):
-    if root == null:
-        root = get_tree().get_root()
-        var game_dir = OS.get_executable_path().get_base_dir()
-        var config_dir = game_dir+"/config/"
-        if FileAccess.file_exists(config_dir+AUTHORNAME_MODNAME_DIR+".txt"):
-            var file = FileAccess.open(config_dir+AUTHORNAME_MODNAME_DIR+".txt", FileAccess.READ)
-            pr("Config exists, reading...")
-            var ln = file.get_line()
-            while ln!="":
-                if ln.substr(0,1)!="#":
-                    var set = ln.split("=")
-                    if float(set[1])!=0:
-                        config[set[0]] = float(set[1])
-                    else:
-                        config[set[0]] = (true if set[1].to_lower()=="true" else false)
-                ln = file.get_line()
-            file.close()
-        else:
-            pr("Config does not exist, creating...")
-            var da = DirAccess.open(game_dir)
-            if not da.dir_exists("config"):
-                da.make_dir("config")
-            var file = FileAccess.open(config_dir+AUTHORNAME_MODNAME_DIR+".txt", FileAccess.WRITE)
-            file.store_string("upscaleResolution="+("true" if config["upscaleResolution"] else "false"))
-            file.store_string("\nwideScreen="+("true" if config["wideScreen"] else "false"))
-            file.store_string("\nrenderScale="+str(config["renderScale"]))
-            file.store_string("\n# Render scale is currently only available for wideScreen")
-            file.store_string("\n# Enabling the setting will override upscaleResolution")
-            file.store_string("\n# -1: Match window resolution\n# 1: 960x540 (Vanilla)\n# 1.5: 1440x810 (1.5x scale)\n# 2: 1920x1080 (2x scale)\n# 4: 3840x2160 (4x scale)\n#\n# And so on...")
-            file.close()
-        pr("Ready for action.")
-        if config["upscaleResolution"]:
-            root.content_scale_mode=1
-    else:
-        var curScene = get_tree().get_current_scene().name
-        if curScene != lastScene and curScene == "menu":
-            pr("Detected menu start, creating custom viewblocker.")
-            title_passed = false
-        lastScene = curScene
-        if config["wideScreen"]:
-            if not title_passed:
-                var blk = safe_get(customBlock)
-                if not blk:
-                    var par = safe_get(copy_parent)
-                    if par:
-                        blk = safe_get(viewBlocker)
-                        if blk:
-                            blk = blk.duplicate()
-                            blk.name = "customBlock"
-                            blk.color = Color(0,0,0,1)
-                            blk.z_index = -1
-                            par.add_child(blk)
-                        else:
-                            pr("[WARN] Could not find viewblocker in the first frame.")
-                    else:
-                        pr("[WARN] Could not find viewblocker parent in the first frame.")
-                if blk:
-                    var rat = safe_get(ratchet)
-                    if not rat.visible:
-                        title_passed = true
-                        blk.queue_free()
-                        pr("Removing custom viewblocker.")
-            var sz = root.get_size()
-            var szX = float(sz[0])
-            var szY = float(sz[1])
-            var mul = 540.0/(540*config["renderScale"])
-            if config["renderScale"] == -1:
-                mul = 0.5
-            var aspectX = szX/szY
-            var canvSize = szY/540.0*mul
-            var relAspX = (aspectX-(16.0/9.0))
-            if curScene == "mp_lobby":
-                root.content_scale_size = Vector2i(960,540)
-                root.set_canvas_transform(Transform2D(Vector2(1,0),Vector2(0,1),Vector2(0,0)))
-                root.content_scale_mode=2
-            else:
-                root.content_scale_size = Vector2i(szX*mul,szY*mul)
-                root.set_canvas_transform(Transform2D(Vector2(canvSize,0),Vector2(0,canvSize),Vector2((relAspX/2.0)*szY*mul,0)))
-                root.content_scale_mode=1
-            for i in forceCoverTargets:
-                var targ = safe_get(i)
-                if targ:
-                    targ.position = Vector2(-2147483647,-2147483647)
-                    targ.scale = Vector2(2147483647,2147483647)
-        elif config["upscaleResolution"]:
-            root.content_scale_factor=1
-            var post = safe_get(forceCoverTargets[0])
-            if post:
-                post.position = Vector2i(-10,-10)
-                post.size = Vector2i(60,60)
+	var curScene = get_tree().get_current_scene().name
+	if curScene != lastScene and curScene == "menu":
+		pr("Detected menu start, creating custom viewblocker.")
+		title_passed = false
+	lastScene = curScene
+	
+	if not title_passed:
+		var blk = safe_get(customBlock)
+		if not blk:
+			var par = safe_get(copy_parent)
+			if par:
+				blk = safe_get(viewBlocker)
+				if blk:
+					blk = blk.duplicate()
+					blk.name = "customBlock"
+					blk.color = Color(0,0,0,1)
+					blk.z_index = -1
+					par.add_child(blk)
+				else:
+					pr("[WARN] Could not find viewblocker in the first frame.")
+			else:
+				pr("[WARN] Could not find viewblocker parent in the first frame.")
+		if blk:
+			var rat = safe_get(ratchet)
+			if not rat.visible:
+				title_passed = true
+				blk.queue_free()
+				pr("Removing custom viewblocker.")
+	
+	var ratio = 16.0/9.0
+	
+	var sz = Vector2(root.get_size())
+	if not config["wideScreen"] or sz.x < sz.y * ratio:
+		sz.x = sz.y * ratio
+	var mul = config["renderScale"]
+	if config["renderScale"] == 0:
+		mul = 0.5
+	sz = sz * vanilla_size.y / sz.y
+	var aspectX = sz.x/sz.y
+	var canvSize = sz.y/float(vanilla_size.y)*mul
+	var relAspX = (aspectX-ratio)
+		
+	root.content_scale_size = Vector2i(sz * mul)
+	root.set_canvas_transform(Transform2D(Vector2(canvSize,0),Vector2(0,canvSize),Vector2((relAspX/2.0)*sz.y*mul,0)))
+	
+	for i in forceCoverTargets:
+		var targ = safe_get(i)
+		if targ:
+			targ.position = Vector2(-2147483647,-2147483647)
+			targ.scale = Vector2(2147483647,2147483647)
